fea5cc9812a39b7637aca97e86c2ff17243f1c5e
arrecis59 <arrecis59@gmail.com>
2018-08-29 13:03:29 -0600

    -

--- prototipoLogistica/dashboard.Designer.cs
+++ prototipoLogistica/dashboard.Designer.cs
@@ -63,7 +63,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(452, 113);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Logistica";
+            this.label2.Text = "Logística";
             // 
             // pictureBox1
             // 
